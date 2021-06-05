c188ebd97fca7d9ecf40f735e476509864ae1570
Alencar-Bianca <alencar-bianca@hotmail.com>
2021-06-05 13:35:01 -0300

    Create the main-header

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <title>Cadastro</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="shortcut icon" href="image/favicon.svg" type="image/x-icon">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;300&family=Stint+Ultra+Condensed&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/colors.css">
+
+    <link rel="stylesheet" href="style/components/main-float.css">
+    <link rel="stylesheet" href="style/components/main-display.css">
+    <link rel="stylesheet" href="style/components/main-header.css">
+    <link rel="stylesheet" href="style/components/main-title.css">
+    
+</head>
+<body>
+    <header class="main-header">
+            <div class="main-display">
+                        <div class="logo -left">
+                            <h1 class="main-title">Alencar</h1>
+                        </div>
+
+                        <div class="menu-desktop -right">
+                            <ul class="ul">
+                                <li class="li"><a class="a" href="">Home</a> </li>
+                                <li class="li"><a class="a" href="">Serviços</a> </li>
+                                <li class="li"><a class="a" href="">Sobre</a> </li>
+                                <li class="li"><a class="a" href="">Contato</a></li>
+                            </ul>
+                        </div>
+            </div>
+            <div class="clear"></div>
+    </header>
+   
+    <script src="https://kit.fontawesome.com/c53d477a6e.js" crossorigin="anonymous"></script>
+</body>
+</html>
